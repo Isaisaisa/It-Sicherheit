c627f93c831a656c953a08f2aa6c0e046f1df892
Louisa Spahl <louisa.spahl@haw-hamburg.de>
2016-05-15 15:45:35 +0200

    initial commit

---
+++ it-sicherheit-secure-passwords/config/environment.rb
@@ -0,0 +1,7 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+ENV['RAILS_RELATIVE_URL_ROOT'] = "/myapp"
