c4d12e4ef72109b255bbe1124efab27fbba0f991
Torben Haug <claustorbenhaug@hotmail.de>
2016-05-16 19:11:23 +0200

    remove unnessary things

--- it-sicherheit-secure-passwords/config/routes.rb
+++ it-sicherheit-secure-passwords/config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root 'static_pages#home'
-  get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
