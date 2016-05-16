8646a011d0b2000f71fb4b62b73ddc3746d3e5bc
Torben Haug <claustorbenhaug@hotmail.de>
2016-05-16 16:01:40 +0200

    create Users

--- it-sicherheit-secure-passwords/app/controllers/users_controller.rb
+++ it-sicherheit-secure-passwords/app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the IT-Sicherheit Secure Login!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
