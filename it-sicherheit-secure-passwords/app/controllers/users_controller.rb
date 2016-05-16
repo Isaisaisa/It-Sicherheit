cfb741fb483c63ec213194fc1eb3b21f20768ff8
Torben Haug <claustorbenhaug@hotmail.de>
2016-05-16 18:06:34 +0200

    first user implementation

--- it-sicherheit-secure-passwords/app/controllers/users_controller.rb
+++ it-sicherheit-secure-passwords/app/controllers/users_controller.rb
@@ -1,8 +1,31 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:index, :destroy]
+
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the IT-Sicherheit Secure Login!"
       redirect_to @user
     else
@@ -18,10 +41,34 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
 end
--- it-sicherheit-secure-passwords/db/seeds.rb
+++ it-sicherheit-secure-passwords/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name:  "Test User",
+             email: "test@user.de",
+             password:              "test123",
+             password_confirmation: "test123")
+
+User.create!(name:  "Test User2",
+             email: "test2@user.de",
+             password:              "test1234",
+             password_confirmation: "test1234")
+
+User.create!(name:  "Torben",
+             email: "torben@user.de",
+             password:              "test112",
+             password_confirmation: "test112",
+             admin: true)
+
+User.create!(name:  "Louisa",
+             email: "louisa@user.de",
+             password:              "test122",
+             password_confirmation: "test122",
+             admin: true)
