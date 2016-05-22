59690f39bb02635d95f9d3fdbdace0df71c69a9c
Torben Haug <claustorbenhaug@hotmail.de>
2016-05-22 19:57:08 +0200

    update seeds

--- it-sicherheit-secure-passwords/db/seeds.rb
+++ it-sicherheit-secure-passwords/db/seeds.rb
@@ -8,30 +8,30 @@
 
 User.create!(name:  "Test User",
              email: "test@user.de",
-             password:              "test123",
-             password_confirmation: "test123",
+             password:              "T3$t1234",
+             password_confirmation: "T3$t1234",
              activated: true,
              activated_at: Time.zone.now)
 
 User.create!(name:  "Test User2",
              email: "test2@user.de",
-             password:              "test1234",
-             password_confirmation: "test1234",
+             password:              "T3$t1234",
+             password_confirmation: "T3$t1234",
              activated: true,
              activated_at: Time.zone.now)
 
 User.create!(name:  "Torben",
              email: "torben@user.de",
-             password:              "test112",
-             password_confirmation: "test112",
+             password:              "T3$t1234",
+             password_confirmation: "T3$t1234",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
 
 User.create!(name:  "Louisa",
              email: "louisa@user.de",
-             password:              "test122",
-             password_confirmation: "test122",
+             password:              "T3$t1234",
+             password_confirmation: "T3$t1234",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
