0750c3780baf8de9c0d0b4eb6f3f06838f335176
Torben Haug <claustorbenhaug@hotmail.de>
2016-06-05 16:02:34 +0200

    add  to_s

--- it-sicherheit-secure-passwords/app/controllers/sessions_controller.rb
+++ it-sicherheit-secure-passwords/app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
   def createcert
     if request.env['SSL_CLIENT_VERIFY'] == "SUCCESS"
-      puts "userid: " + request.env['SSL-Client-CN']
+      puts "userid: " + request.env['SSL-Client-CN'].to_s
       user = User.find_by(id: request.env['SSL-Client-CN'])
       loginuser(user)
     else
